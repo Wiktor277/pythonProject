dbac839be241232597c6b53d2c8271b0bd027020
Wiktor Powąski <victotmi0727@gmail.com>
2026-03-17 20:15:49 +0100

    classes 2

---
+++ 2nd exercise.py
@@ -0,0 +1,17 @@
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def shout(self):
+        print(f'my name is {self.name} and Im {self.age} years old its nice to meet you')
+
+# 1 Instantiate the Cat object with 3 cats
+orangecat = Cat("cookie", 4)
+bigcat = Cat("tomtom", 8)
+greycat = Cat("lala", 2)
+
+def shout(self):
+    print(f'my name is {self.name}')
+
+(orangecat.shout())
